82f0770ae1be9c53f50541adb97df175e33a3acd
LucasAndrew <lucasandrew0120@outlook.com>
2026-02-08 20:46:44 +0800

    feat: 关键Token和链接分离，实现用户自定义

---
+++ example.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+        name: '', // 站长名字
+        avartar: '', // 站长头像
+        github_username: '', // GitHub 用户名
+        icon: '', // 网站图标
+        title: "", // 网站标题
+        aphorism1: '', // 主页个人简介下方的格言或座右铭
+        aphorism2: '', // 主页底部的格言或座右铭
+        qweatherKey: '', // 和风天气 API Key
+        amapKey: '', // 高德地图 API Key
+        umami_domain: '', // Umami 统计脚本的域名（不包含 /script.js）
+        umami_id: '', // Umami 统计的 Website ID
+        font: '', // 个性化字体的 CSS 的CDN链接
+        font_family: '', // 个性化字体的字体名称（需要与 CSS 中定义的字体名称一致）
+        background: '', // 主页背景图片的 URL
+        echoApiUrl: '' // Echo API 的 URL
+};
