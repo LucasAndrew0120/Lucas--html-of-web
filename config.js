8ca01b3760455402d78562927760908c776f296c
LucasAndrew <lucasandrew0120@outlook.com>
2026-02-10 19:27:26 +0800

    测试

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+        name: 'LucasAndrew', // 站长名字
+        avartar: 'https://pic1.imgdb.cn/item/68fe309a3203f7be00a0c198.png', // 站长头像
+        github_username: 'LucasAndrew0120', // GitHub 用户名
+        icon: 'https://pic1.imgdb.cn/item/68fe309a3203f7be00a0c198.png', // 网站图标
+        title: "Lucas的忘忧斋", // 网站标题
+        aphorism1: '南风知我意，吹梦到西洲', // 主页个人简介下方的格言或座右铭
+        aphorism2: '琴屿飘灯，山海共生 · 与 AI 共同编织这段代码', // 主页底部的格言或座右铭
+        qweatherKey: '2b90ecc447f9444db695935fdaa7e8bb', // 和风天气 API Key
+        amapKey: 'ac70c2ede02ff7cb29a5a874c9ac498d', // 高德地图 API Key
+        umami_domain: 'https://umami.lris625.top', // Umami 统计脚本的域名（不包含 /script.js）
+        umami_id: 'ef29f7c2-2ab1-4900-a37c-16c998700394', // Umami 统计的 Website ID
+        font: 'https://chinese-fonts-cdn.deno.dev/packages/lywkpmydb/dist/LXGWWenKaiScreen/result.css', // 个性化字体的 CSS 的CDN链接
+        font_family: 'LXGW WenKai Screen', // 个性化字体的字体名称（需要与 CSS 中定义的字体名称一致）
+        background: 'https://pic1.imgdb.cn/item/6943a7742ee916d1a3aee235.webp', // 主页背景图片的 URL
+        echoApiUrl: 'https://ech0.lris625.top/api/echo/page' // Echo API 的 URL
+};
